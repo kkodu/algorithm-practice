df865cad2e6e54db342fd4ec5ddd0f900c11f4ae
Wooyoung Ko <nawy1130@gmail.com>
2018-06-27 16:31:53 +0900

    Update Year2016.java

--- Year2016.java
+++ Year2016.java
@@ -1,40 +1,40 @@
 public class Year2016 {
 	public static String solution(int a, int b) {
 		int[] month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-      	int sum = 0;
-      	int day;
-      	String answer = "";
+		int sum = 0;
+		int day;
+		String answer = "";
 
-      	for (int i = 1; i < a; i++) {
-          	sum += month[i-1];
-      	}
-      	sum += b;
+		for (int i = 1; i < a; i++) {
+			sum += month[i-1];
+		}
+		sum += b;
 
-      	day = sum % 7;
-      	switch (day) {
-          	case 1:
-              	answer = "FRI";
-              	break;
-          	case 2:
-              	answer = "SAT";
-              	break;
-          	case 3:
-            	answer = "SUN";
-              	break;
-          	case 4:
-              	answer = "MON";
-              	break;
-          	case 5:
-              	answer = "TUE";
-              	break;
-          	case 6:
-              	answer = "WED";
-              	break;
-          	default:
-              	answer = "THU";
-      	}
+		day = sum % 7;
+		switch (day) {
+			case 1:
+				answer = "FRI";
+				break;
+			case 2:
+				answer = "SAT";
+				break;
+			case 3:
+				answer = "SUN";
+				break;
+			case 4:
+				answer = "MON";
+				break;
+			case 5:
+				answer = "TUE";
+				break;
+			case 6:
+				answer = "WED";
+				break;
+			default:
+				answer = "THU";
+		}
 
-      	return answer;
+		return answer;
   	}
 
 	public static void main(String[] args) {
